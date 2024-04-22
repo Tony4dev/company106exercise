7a9a7d40c70f0dd9eb5243fa6064a8832a227c06
Tony4Dev <tony4dev@pm.me>
2024-04-22 15:33:35 +0200

    update textation

--- src/main/java/org/example/service/EmployeeReportService.java
+++ src/main/java/org/example/service/EmployeeReportService.java
@@ -25,7 +25,6 @@ public class EmployeeReportService {
         printOverpayedEmployees(employeeReports);
         printEmptyLine();
         printEmployeesWithTooLongReportingLine(employeeReports);
-
     }
 
     private Set<EmployeeReport> createReports() {
@@ -70,7 +69,7 @@ public class EmployeeReportService {
     }
 
     private static void printEmployeesWithTooLongReportingLine(Set<EmployeeReport> employeeReports) {
-        System.out.println("Following managers have too long reporting line: ");
+        System.out.println("Following employees have too long reporting line: ");
 
         employeeReports.stream()
                 .filter(r -> r.reportingLineLength() > MAX_OPTIMAL_REPORTING_LINE_LENGTH)
